633d6ff9ddd4dd1add5fdd9496fc5a141d59dd17
choowx <choowx@users.noreply.github.com>
2023-01-05 06:10:17 +0000

    Fix styling

--- tests/SvgTest.php
+++ tests/SvgTest.php
@@ -2,7 +2,6 @@
 
 use Choowx\RasterizeSvg\Svg;
 use function Spatie\Snapshots\assertMatchesFileHashSnapshot;
-use function Spatie\Snapshots\assertMatchesSnapshot;
 
 beforeEach(function () {
     $svg = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /> </svg>';
